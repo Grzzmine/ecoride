5560f1ef17a507e6c384ce84dad9d12cbdee8145
Grzzmine <thierry.filon@hotmail.fr>
2025-05-31 11:53:24 +0200

    🚀 Initialisation du projet EcoRide (branche develop) ➕ Ajout du fichier .gitignore pour ignorer les fichiers sensibles et temporaires Maj Avis + Reservation

--- classes/Avis.php
+++ classes/Avis.php
@@ -1,1 +1,39 @@
 <?php
+// Classe Avis
+
+class Avis
+{
+    public $auteur;
+    public $chauffeur;
+    public $note;
+    public $commentaire;
+    public $valide;
+
+    public function __construct($auteur, $chauffeur, int $note, string $commentaire)
+        {
+        $this->auteur = $auteur;
+        $this->chauffeur = $chauffeur ;
+        if($note < 1 || $note > 5){
+            throw new Exception("Note invalide : La note doit être comprise entre 1 et 5.");
+        }
+        $this->note = $note;
+        $this->commentaire = $commentaire;
+        $this->valide = false;
+        }
+
+    public function validerAvis()
+    {
+        $this->valide = true;
+    }
+
+    public function refuserAvis()
+    {
+        $this->valide = false;
+    }
+
+    public function afficherAvis()
+    {
+        return "Avis de {$this->auteur->pseudo} sur {$this->chauffeur->pseudo} : {$this->note} / 5, commentaire : {$this->commentaire}";
+    }
+
+}
--- classes/Reservation.php
+++ classes/Reservation.php
@@ -1,1 +1,27 @@
 <?php
+// Classe Reservation
+
+class Reservation
+{
+    public $passager;
+    public $trajet;
+    public $dateReservation;
+    public $statut;
+
+    public function __construct($passager, $trajet)
+    {
+        $this->passager = $passager;
+        $this->trajet = $trajet;
+        $this->dateReservation = date ("d/m/Y H:i:s");
+        $this->statut = "confirmé";
+    }
+
+    public function annuler()
+        {
+        $this->statut = "annulé";
+        }
+    public function afficherDetails()
+    {
+        return "Réservation de {$this->passager->pseudo} pour le trajet de {$this->trajet->villeDepart} à {$this->trajet->villeArrivee} le {$this->trajet->date} à {$this->trajet->heureDepart} – Statut : {$this->statut}";
+    }
+}
